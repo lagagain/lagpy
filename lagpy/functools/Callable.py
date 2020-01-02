4356acd7695878020a296e89b7dc54a86dc9fd84
lagagain <lagagain@email.com>
2020-01-02 13:29:04 +0800

    Make CallableWrapper inherit Callable and Proxy.

--- lagpy/functools/Callable.py
+++ lagpy/functools/Callable.py
@@ -1,18 +1,4 @@
-class CallableWrapper:
-    def __init__(self, wrap):
-        self.wrap = wrap
-    def __call__(self, f=None, *args, **kargs):
-        if f == None:
-            return self.wrap
-        result = f(self.wrap, *args, **kargs)
-        class __callable(type(result), Callable):
-            pass
-        return __callable(result, *args, **kargs)
-    @staticmethod
-    def new(wrap, *args, **kargs):
-        class __callable(type(wrap), Callable):
-            pass
-        return __callable(wrap, *args, **kargs)
+from .Proxy import Proxy
 
 
 class CallableMeta(type):
@@ -32,7 +18,33 @@ class CallableMeta(type):
 class Callable(metaclass = CallableMeta):
     pass
 
-#''' Example
+class CallableWrapper(Proxy, Callable):
+    def __init__(self, wrap, *args, **kargs):
+        self.wrap = wrap
+        super().__init__(wrap, *args, **kargs)
+    def __call__(self, f=None, *args, **kargs):
+        if f == None:
+            return self.wrap
+        result = f(self.wrap, *args, **kargs)
+        __callable = CallableWrapper.__genClass(result)
+        return __callable(result, *args, **kargs)
+    @staticmethod
+    def new(wrap, *args, **kargs):
+        __callable = CallableWrapper.__genClass(wrap)
+        return __callable(wrap, *args, **kargs)
+    @staticmethod
+    def __genClass(wrap, *args, **kargs):
+        try:
+            class __callable(type(wrap), Callable):
+                pass
+            return __callable
+        except TypeError as e:
+            return CallableWrapper
+
+
+
+''' Example
 class CallableList(list, Callable):
     def hello(self):
         print("Hello World")
+'''
